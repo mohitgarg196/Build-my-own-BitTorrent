0d5b4dad7dc878f07913b5b116b135c76b4f81dc
Mohit Garg <mohitgarg8910@gmail.com>
2024-09-10 12:03:38 +0530

    4th stage passed

--- src/Main.cpp
+++ src/Main.cpp
@@ -74,39 +74,24 @@ std::tuple<json, std::string> decode_bencoded_value(const std::string &encoded_v
 
 int main(int argc, char *argv[])
 {
-    // Flush after every std::cout / std::cerr
-    std::cout << std::unitbuf;
-    std::cerr << std::unitbuf;
-
-    if (argc < 2)
-    {
+    if (argc < 2) {
         std::cerr << "Usage: " << argv[0] << " decode <encoded_value>" << std::endl;
         return 1;
     }
-
     std::string command = argv[1];
-
-    if (command == "decode")
-    {
-        if (argc < 3)
-        {
+    if (command == "decode") {
+        if (argc < 3) {
             std::cerr << "Usage: " << argv[0] << " decode <encoded_value>" << std::endl;
             return 1;
         }
-        // You can use print statements as follows for debugging, they'll be visible when running tests.
-        // std::cout << "Logs from your program will appear here!" << std::endl;
-
-        // Uncomment this block to pass the first stage
-
-        std::string encoded_value = argv[2]; // this is the change
-        json decoded_value = decode_bencoded_value(encoded_value);
+        std::string encoded_value = argv[2];
+        json decoded_value;
+        std::tie(decoded_value, std::ignore) = decode_bencoded_value(encoded_value);
+//        json decoded_value = decode_bencoded_value(encoded_value);
         std::cout << decoded_value.dump() << std::endl;
-    }
-    else
-    {
+    } else {
         std::cerr << "unknown command: " << command << std::endl;
         return 1;
     }
-
     return 0;
 }
